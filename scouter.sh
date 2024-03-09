e9a0355512279248737a4f03083a5018c4117b5e
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2024-03-09 08:41:46 -0800

    Allow script to move past the getopt check

--- scouter.sh
+++ scouter.sh
@@ -6,6 +6,7 @@ SHORT_OPTIONS=''
 getopt --test
 getopt_state=$?
 
-[ ! $getopt_state -eq 4 ] && echo "getopt --test error code $getopt_state, when it should be 4."; exit 1
+[ ! $getopt_state -eq 4 ] && echo "getopt --test error code $getopt_state, when it should be 4." && exit 1
 
 echo "getopt --test is $getopt_state! Moving forward."
+
